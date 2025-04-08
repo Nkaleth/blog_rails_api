9125908330a73e0e1d709328b7d303adc2c3bfdf
Nkaleth <nseguralu@gmail.com>
2025-04-08 12:14:20 -0500

    CREATE methods create and update also include them in the routes and fix the create post spec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render json: @post, status: :ok
   end
+
+  # POST /posts
+  def create
+    @post = Post.create!(create_params)
+    render json: @post, status: :created
+  end
+
+  # POST /posts/{id}
+  def update
+    @post = Post.find(params[:id])
+    @post.update!(update_params)
+    render json: @post, status: :ok
+  end
+
+  private
+
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/health", to: "health#health"
 
-  resources :posts, only: [ :index, :show  ]
+  resources :posts, only: [ :index, :show, :create, :update  ]
 end
