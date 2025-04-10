4634ad0c0b3e88e40368239d649e6b22e91c5e05
Nkaleth <nseguralu@gmail.com>
2025-04-09 20:40:16 -0500

    ADD new tests for search endpoint and create new factory for posts called published_posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
-require 'debug'
 
 RSpec.describe "Posts endpoints", type: :request do
   describe "GET /posts " do
-    before { get '/posts' }
-
     it "should return OK" do
+      get '/posts'
       payload = JSON.parse(response.body)
       expect(payload).to be_empty
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "Search" do
+    let!(:hola_mundo) { create(:published_post, title: 'Hola Mundo') }
+    let!(:hola_rails) { create(:published_post, title: 'Hola Rails') }
+    let!(:curso_rails) { create(:published_post, title: 'Curso Rails') }
+
+    it "Should filter posts by title" do
+      get '/posts?search=Hola'
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload.size).to eql(2)
+      expect(payload.map { |p| p["id"] }.sort).to eq([ hola_mundo.id, hola_rails.id ].sort)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+
 
   describe "with data in the DB" do
     let!(:posts) { create_list(:post, 10, published: true) }
