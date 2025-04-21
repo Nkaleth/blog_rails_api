ce67179a16d5dd25f64808ee48b33c6f42bc874a
Nkaleth <nseguralu@gmail.com>
2025-04-21 18:46:09 -0500

    Implement test for update posts with and without authentication, add an error management when a record is not found, create a concern called secured.rb with authenticate_user! method and delete repeated and old tests for create and update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  include Secured
   before_action :authenticate_user!, only: [ :create, :update ]
 
   rescue_from Exception do |e|
@@ -7,6 +8,10 @@ class PostsController < ApplicationController
 
   rescue_from ActiveRecord::RecordInvalid do |e|
     render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { error: e.message }, status: :not_found
   end
 
   # GET /posts
@@ -50,19 +55,5 @@ class PostsController < ApplicationController
 
   def update_params
     params.require(:post).permit(:title, :content, :published)
-  end
-
-  def authenticate_user!
-    token_regex = /Bearer (\w+)/
-    headers = request.headers
-
-    if headers["Authorization"].present? && headers["Authorization"].match(token_regex)
-      token = headers["Authorization"].match(token_regex)[1]
-      if (Current.user = User.find_by_auth_token(token))
-        return
-      end
-    end
-
-    render json: { error: "Unauthorized" }, status: :unauthorized
   end
 end
--- spec/requests/private_posts_spec.rb
+++ spec/requests/private_posts_spec.rb
@@ -5,12 +5,13 @@ require 'rails_helper'
 RSpec.describe 'Posts with authentication', type: :request  do
   let(:user) { create(:user) }
   let(:other_user) { create(:user) }
-  let(:user_post) { create(:post, :user_id, user.id) }
+  let(:user_post) { create(:post, user_id: user.id) }
   let(:other_user_post) { create(:post, user_id: other_user.id, published: true) }
   let(:other_user_post_draft) { create(:post, user_id: other_user.id, published: false) }
   let!(:auth_headers)  { { 'Authorization' => "Bearer #{user.auth_token}" } }
   let!(:other_auth_headers) { { 'Authorization' => "Bearer #{other_user.auth_token}" } }
   let!(:create_params) { { "post" => { "title" => "title", "content" => "content", "published" => true } } }
+  let!(:update_params) { { "post" => { "title" => "title", "content" => "content", "published" => true } } }
 
   describe "GET /posts/{id}" do
     context "with valid auth" do
@@ -74,6 +75,32 @@ RSpec.describe 'Posts with authentication', type: :request  do
   end
 
   describe "PUT /posts" do
+    context "with valid auth" do
+      context "when updating user's post" do
+        before { put "/posts/#{user_post.id}", params: update_params, headers: auth_headers }
+        context "Payload" do
+          subject { payload }
+          it { is_expected.to include(:id, :title, :content, :published, :author) }
+          it { expect(payload[:id]).to eql(user_post.id)  }
+        end
+        context "Response" do
+          subject { response }
+          it { is_expected.to have_http_status(:ok) }
+        end
+      end
+
+      context "When updating other's user post" do
+        before { put "/posts/#{other_user_post.id}", params: update_params, headers: auth_headers }
+        context "Payload" do
+          subject { payload }
+          it { is_expected.to include(:error) }
+        end
+        context "Response" do
+          subject { response }
+          it { is_expected.to have_http_status(:not_found) }
+        end
+      end
+    end
   end
 
   private
